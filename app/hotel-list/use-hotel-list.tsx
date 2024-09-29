abfec0e1368a1e81c164bec24a15ef3c9228de85
Josiah Yeow <josiah@propps.com>
2024-09-29 21:21:45 +1000

    refactor: move sort functions out to remove useEffect

--- app/hotel-list/use-hotel-list.tsx
+++ app/hotel-list/use-hotel-list.tsx
@@ -1,31 +1,31 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Hotel } from "./get-hotels";
 
 type SortBy = "price-high-low" | "price-low-high";
 
 export function useHotelList(data: Hotel[]) {
   const [sortBy, setSortBy] = useState<SortBy>("price-high-low");
-  const [hotels, setHotels] = useState(data);
+  const [hotels, setHotels] = useState(
+    [...data].sort(sortHotelPriceDescendingFn)
+  );
 
   const sortPriceHighToLow = () => {
-    const sortedHotels = [...hotels].sort(
-      (a, b) => b.offer.displayPrice.amount - a.offer.displayPrice.amount
-    );
+    const sortedHotels = [...hotels].sort(sortHotelPriceDescendingFn);
     setHotels(sortedHotels);
     setSortBy("price-high-low");
   };
 
   const sortPriceLowToHigh = () => {
-    const sortedHotels = [...hotels].sort(
-      (a, b) => a.offer.displayPrice.amount - b.offer.displayPrice.amount
-    );
+    const sortedHotels = [...hotels].sort(sortHotelPriceAscendingFn);
     setHotels(sortedHotels);
     setSortBy("price-low-high");
   };
 
-  useEffect(() => {
-    sortPriceHighToLow();
-  }, []);
-
   return { hotels, sortPriceHighToLow, sortPriceLowToHigh, sortBy };
 }
+
+const sortHotelPriceDescendingFn = (a: Hotel, b: Hotel) =>
+  b.offer.displayPrice.amount - a.offer.displayPrice.amount;
+
+const sortHotelPriceAscendingFn = (a: Hotel, b: Hotel) =>
+  a.offer.displayPrice.amount - b.offer.displayPrice.amount;

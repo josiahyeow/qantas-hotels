318cf4cc51df505672df95281e181dbacc1982cf
Josiah Yeow <josiah@propps.com>
2024-09-29 22:06:06 +1000

    feat: add get hotel data function

--- app/hotel-list/get-hotels.tsx
+++ app/hotel-list/get-hotels.tsx
@@ -1,9 +1,45 @@
-import { promises as fs } from "fs";
+import data from "./hotel-data.json";
 
-export async function getHotels() {
-  const file = await fs.readFile(
-    process.cwd() + "/app/hotel-list/hotel-data.json",
-    "utf8"
-  );
-  return JSON.parse(file);
+export type Hotel = {
+  id: string;
+  property: {
+    propertyId: string;
+    title: string;
+    address: string[];
+    previewImage: {
+      url: string;
+      caption: string;
+      imageType: "PRIMARY";
+    };
+    rating: {
+      ratingValue: number;
+      ratingType: "self" | "star";
+    };
+  };
+  offer: {
+    promotion: {
+      title: string;
+      type: "MEMBER" | "CAMPAIGN";
+    };
+    name: string;
+    displayPrice: {
+      amount: number;
+      currency: "AUD";
+    };
+    savings: {
+      amount: number;
+      currency: "AUD";
+    } | null;
+    cancellationOption: {
+      cancellationType: "NOT_REFUNDABLE" | "FREE_CANCELLATION";
+    };
+  };
+};
+
+type GetHotelsResponse = {
+  results: Hotel[];
+};
+
+export function getHotels() {
+  return (data as GetHotelsResponse).results;
 }

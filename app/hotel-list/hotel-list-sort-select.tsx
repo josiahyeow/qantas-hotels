7cb70637d3d4679f7dd777dbf7f7f46944e0f216
Josiah Yeow <josiah@propps.com>
2024-10-06 15:33:11 +1100

    feat: style select input

--- app/hotel-list/hotel-list-sort-select.tsx
+++ app/hotel-list/hotel-list-sort-select.tsx
@@ -18,12 +18,13 @@ export function HotelListSortSelect() {
   };
 
   return (
-    <div className="flex flex-row gap-1">
+    <div className="flex flex-row gap-3">
       <label htmlFor="hotel-list-sort-by" className="font-semibold">
         Sort by
       </label>
       <select
         id="hotel-list-sort-by"
+        className="ring rounded ring-gray-300 hover:ring-black hover:cursor-pointer ring-2 px-1 text-sm transition-all"
         value={sortBy}
         onChange={(e) => {
           const value = e.target.value as keyof typeof options;

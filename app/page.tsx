7a7ce95717768d71aae9fd618cbc7978830470af
Josiah Yeow <josiah@propps.com>
2024-10-07 15:44:50 +1100

    feat: don't align center page content

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { HotelList } from "./hotel-list/hotel-list";
 export default function Home() {
   return (
     <div className="m-6 max-w-7xl m-auto">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start p-8">
+      <main className="flex flex-col gap-8 items-start p-8">
         <Header />
         <HotelList />
       </main>
